78fe373c78adbc13ef3fdbc3879694b604f69edb
Abdul Rehman Banday <139254781+rehmanbanday5@users.noreply.github.com>
2023-08-24 09:23:06 +0500

    Add files via upload

---
+++ Class-2/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myname = "Rehman";
+var age = 17;
+var address = "Islamabad";
+//--------------------------------------------------------------------------------
+var stat = ("hi,this is ".concat(myname));
+var abc = "I am Abdul Rehman";
+console.log(abc);
+// how to check datatye -------------------------------------------------------
+//console.log(typeofstat);    at class
+//operators--------------------------------------------------------------------
+var num1 = "10";
+var num2 = "16";
+var add1 = "operators";
+var add2 = "sum of operators will be here";
+// addition--------------------------------------------------------------------
+console.log(num1 + num2);
+//% -------------------------------------------------------------------------
+var number = "18";
+var number2 = "10";
+console.log("number2 % number");
+//assignment operator comparsion --------------------------------------------
+var result = number == number2;
+console.log(result);
---
+++ Class-2/index.ts
@@ -0,0 +1,46 @@
+
+
+let myname:string="Rehman";
+
+ const age:number=17;
+
+var address="Islamabad";
+
+export{}
+
+//--------------------------------------------------------------------------------
+
+let stat = (`hi,this is ${myname}`)
+let abc = "I am Abdul Rehman"
+console.log(abc);
+
+// how to check datatye -------------------------------------------------------
+
+//console.log(typeofstat);    at class
+
+//operators--------------------------------------------------------------------
+
+let num1=`10`;
+let num2=`16`;
+let add1="operators"
+let add2="sum of operators will be here";
+
+
+// addition--------------------------------------------------------------------
+
+console.log(num1+num2);
+
+//% -------------------------------------------------------------------------
+
+let number="18";
+let number2="10";
+
+console.log("number2 % number");
+
+
+//assignment operator comparsion --------------------------------------------
+
+
+let result=number==number2;
+console.log(result);
+
